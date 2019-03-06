ee3ee16957094b923e8bc778fd8a74839fdc6f49
Carlos <carlosreanol@gmail.com>
2019-03-06 15:53:09 +0100

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -6,7 +6,8 @@ const jwt = require('jsonwebtoken');
 
 let verificaToken = (req, res, next) => {
 
-    let token = req.get('token');
+    //   let token = req.get('token');
+    let token = req.query.token ? req.query.token : req.get('token');
 
     jwt.verify(token, process.env.SEED, (err, decoded) => {
 
@@ -48,7 +49,33 @@ let verificaAdmin_Role = (req, res, next) => {
 
 }
 
+// ============================
+// Verificar token para imagen
+// ============================
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.Usuario;
+        next();
+
+    });
+
+}
+
+
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
